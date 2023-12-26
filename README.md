0eed0ed5d484c0de339f4d22f8b620ed83a5df8d
adieb <mrizqiamiruladieb@gmail.com>
2023-12-26 14:42:48 +0700

    login test selenium java

--- README.md
+++ README.md
@@ -9,3 +9,21 @@ Web Automation <br>
 <li><b>Bdd-Tdd : </b>Cucumber Java (BDD & TDD)</li>
 <li><b>GeRaTa : </b>Generate Random Data &rarr; Automate Registration Form</li>
 <li><b>POM : </b>Page Object Modelling</li>
+
+### Problems & Solutions
+___
+#### 1. Resolving java: cannot access org.openqa.selenium.WebDriver
+- `Issue details` : class file has wrong version 55.0, should be 52.0
+- `Solution` : Di file *pom.xml* &rarr; *dependencies* &rarr; *dependency selenium-java* &rarr; **Downgrade the Version**
+
+#### 2. Resolving //*[contains(text(), 'string')] not working in XPath
+- `Solution` : //button[text()[contains(., 'Login')]]
+- `References` : [Ref 1](https://stackoverflow.com/questions/71253563/why-is-containstext-string-not-working-in-xpath) 
+dan [Ref 2](https://stackoverflow.com/questions/3655549/xpath-containstext-some-string-doesnt-work-when-used-with-node-with-more/71255563#71255563)
+
+#### 3. Resolving wait.until(ExpectedConditions.visibilityOf Element1 OR Element2)
+- `Solution` : wait.until(
+  ExpectedConditions.or(ExpectedConditions.visibilityOfElementLocated(By.xpath("blabla1")),
+  ExpectedConditions.visibilityOfElementLocated(By.xpath("blabla2")))
+  );
+- `References` : [Klik di sini](https://stackoverflow.com/questions/14840884/wait-untilexpectedconditions-visibilityof-element1-or-element2)
---
+++ src/test/java/auth/Login.java
@@ -0,0 +1,97 @@
+package auth;
+
+import config.env_target;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Login extends env_target {
+    @Test //successful login
+    public void main(){
+        //Set driver location path
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\drivers\\chromedriver.exe");
+        //Maximize driver
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //Set url
+        driver.get(baseUrl);
+        //Set waktu tunggu
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit'][text()[contains(., 'Login')]]"))
+        );
+        //Set element locate
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@type='submit'][text()[contains(., 'Login')]]")).click();
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//h6[text()[contains(., 'Dashboard')]]"))
+        );
+        //Quit chrome
+        driver.quit();
+    }
+
+    @Test //failed to log in (invalid username or password)
+    public void failed(){
+        //Set driver location path
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\drivers\\chromedriver.exe");
+        //Maximize driver
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //Set url
+        driver.get(baseUrl);
+        //Set waktu tunggu
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit'][text()[contains(., 'Login')]]"))
+        );
+        //Set element locate
+        driver.findElement(By.name("username")).sendKeys("username_salah");
+        driver.findElement(By.name("password")).sendKeys("password_salah");
+        driver.findElement(By.xpath("//button[@type='submit'][text()[contains(., 'Login')]]")).click();
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()[contains(., 'Invalid credentials')]]"))
+        );
+        //Quit chrome
+        driver.quit();
+    }
+
+    @Test //failed to log in (blank username and password)
+    public void failed2(){
+        //Set driver location path
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\drivers\\chromedriver.exe");
+        //Maximize driver
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //Set url
+        driver.get(baseUrl);
+        //Set waktu tunggu
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit'][text()[contains(., 'Login')]]"))
+        );
+        //Set element locate
+        driver.findElement(By.name("username")).sendKeys("");
+        driver.findElement(By.name("password")).sendKeys("");
+        driver.findElement(By.xpath("//button[@type='submit'][text()[contains(., 'Login')]]")).click();
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.or(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/span")),
+                        ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/span")))
+        );
+        //Quit chrome
+        driver.quit();
+    }
+}
